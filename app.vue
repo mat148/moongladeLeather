0895142c589f1829858b5646df958c567d079f8c
mat148 <mrm1779@gmail.com>
2023-08-17 19:53:10 -0400

    Setup page structure

--- app.vue
+++ app.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="container">
-    
+  <header class="mt-auto flex flex-grow py-4 px-4 bg-teal-500">
+    <div class="flex-grow">
+      <a href="#" target="_blank"><img src="#"/></a>
+    </div>
+    <nav>
+      <ul class="flex">
+        <li class="pl-3"><a href="#">test</a></li>
+        <li class="pl-3"><a href="#">test</a></li>
+        <li class="pl-3"><a href="#">test</a></li>
+      </ul>
+    </nav>
+  </header>
+  <div class="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow">
+    <!-- fixed-width -->
+    <div class="w-fixed w-full flex-shrink flex-grow-0 px-4">
+        <div class="sticky top-0 p-4 w-full h-full">
+          <img src="#"/>
+        </div>
+    </div>
+    <nav class="w-fixed w-full flex-shrink flex-grow-0 px-2">
+        
+    </nav>
   </div>
+  <footer class="bg-black mt-auto">
+      ...
+  </footer>
 </template>
 
 <script>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      container: {
+        center: true,
+      },
+    },
+  }
